0c12b09eba7888d1f19d360977a61675a15c1b55
Muhamed Abobakr <muhammad.bakr2005@gmail.com>
2025-11-26 14:30:18 +0200

    Fixed path for *

--- backend/src/server.js
+++ backend/src/server.js
@@ -31,7 +31,7 @@ if(process.env.NODE_ENV == "production") {
     app.use(express.static(path.join(__dirname, "../frontend/dist"))); 
 
     // if we get any route other than our api/notes
-    app.get("*", (req, res) => {
+    app.get("/*", (req, res) => {
         res.sendFile(path.join(__dirname, "../frontend/", "dist","/index.html"));
     });
 }
